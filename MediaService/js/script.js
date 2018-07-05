5c0dc9d8c45f27ef5efe965ed943f01daf5c25d2
ImmortalRabbit <demezhan1998@gmail.com>
2018-07-05 19:56:32 +0600

    Add files via upload

--- MediaService/js/script.js
+++ MediaService/js/script.js
@@ -1,12 +1,10 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("mySidenav").style.right = "0";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight= "0";
+    document.getElementById("mySidenav").style.right = "-250px";
     document.body.style.backgroundColor = "white";
 }
 
