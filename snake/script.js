007084706f3f97200f764f143bb5b8c5194a5c3e
Demezhan Marikov <demezhan.marikov@nu.edu.kz>
2020-05-19 03:28:22 +0600

    snake OOP

--- snake/script.js
+++ snake/script.js
@@ -3,221 +3,176 @@ var context = canvas.getContext('2d');
 var grid = 16;
 var count = 0;
 
-
-
-var snakeA = {
-  x: 560,
-  y: 560,
-  // snake velocity. moves one grid length every frame in either the x or y direction
-  dx: grid,
-  dy: 0,
-  // keep track of all grids the snake body occupies
-  cells: [],
-  // length of the snake. grows when eating an apple
-  maxCells: 4
+function Snake(color) {
+  this.color = color;
+  this.x = getRandomInt();
+  this.y = getRandomInt();
+  this.dx = 16;
+  this.dy = 0;
+  this.cells = [];
+  this.maxCells = 4;
 };
 
-var snakeB = {
-  x: 560,
-  y: 560,
-  // snake velocity. moves one grid length every frame in either the x or y direction
-  dx: grid,
-  dy: 0,
-  // keep track of all grids the snake body occupies
-  cells: [],
-  // length of the snake. grows when eating an apple
-  maxCells: 4
-};
-
-var apple = {
-  x: 128,
-  y: 288
-};
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function reset(snake){
-  if(snake == snakeA){
-      snake.dx = -grid;
-      snake.x = 192;
-      snake.y = 192;
-  }else{
-      snake.dx = grid;
-      snake.x = 384;
-      snake.y = 384;
-  }
-  snake.cells = [];
-  snake.maxCells = 4;
-  snake.dy = 0;
-}
-
-function move(snake){
-  snake.x += snake.dx;
-  snake.y += snake.dy;
-}
-
-function moveSnakes(){
-  move(snakeA);
-  move(snakeB);
-}
-
-function snakeHitsWall(snake){
-  if (snake.x < 0 || snake.x >= canvas.width || snake.y < 0 || snake.y >= canvas.height ) {
-    reset(snake);
-  }
-}
-
-function snakesHitWall(){
-  snakeHitsWall(snakeA);
-  snakeHitsWall(snakeB);
-}
-
-function putApple(){
-  apple.x = getRandomInt(0, 30) * grid;
-  apple.y = getRandomInt(0, 30) * grid;
-}
-
-function drawApple(){
-  context.fillStyle = 'red';
-  context.fillRect(apple.x, apple.y, grid-1, grid-1);
-}
-
-function trackSnake(snake){
-  snake.cells.unshift({x: snake.x, y: snake.y});
-}
-
-function trackSnakes(){
-  trackSnake(snakeA);
-  trackSnake(snakeB);
-}
-
-function removeCell(snake){
-  if (snake.cells.length > snake.maxCells) {
-    snake.cells.pop();
-  }
-}
-
-function removeCellsAsWeMoveAway(){
-  removeCell(snakeA);
-  removeCell(snakeB);
-}
-
-function snakeEatsSnake(cell, index, snake){
-  for (var i = index + 1; i < snake.cells.length; i++) {
-    if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-      return true;
+Snake.prototype = {
+  constuctor: Snake,
+  move: function(){
+    this.x += this.dx;
+    this.y += this.dy;
+  },
+  hitsWall: function(){
+    if (this.x < 0 || this.x >= canvas.width || this.y < 0 || this.y >= canvas.height ) {
+      this.reset();
+    }
+  },
+  reset: function(){
+    this.x = getRandomInt();
+    this.y = getRandomInt();
+    this.cells = [];
+    this.maxCells = 4;
+    this.dx = 16
+    this.dy = 0;
+  },
+  track: function(){
+    this.cells.unshift({x: this.x, y: this.y});
+  },
+  removeCell: function(){
+    if (this.cells.length > this.maxCells) {
+      this.cells.pop();
+    }
+  },
+  eats: function(){
+    context.fillStyle = this.color;
+    for (let i = 0; i < this.cells.length; i++) {
+      context.fillRect(this.cells[i].x, this.cells[i].y, 15, 15);
+      if(this.cells[i].x === apple.x && this.cells[i].y === apple.y){
+        this.maxCells++;
+        apple.reset();
+      }
+      if(this.eatsHimself(i)){
+        this.reset();
+      }
+      
+    }
+  },
+  eatsHimself: function(index){
+    for(let j = index + 1; j < this.cells.length; j++){
+      if (this.cells[index].x === this.cells[j].x && this.cells[index].y === this.cells[j].y) {
+        return true;
+      }
+    }
+    return false;
+  },
+  loop: function(){
+    this.move();
+    this.hitsWall();
+    this.track();
+    this.removeCell();
+    this.eats();
+  },
+  left: function(){
+    if(this.dx == 0){
+      this.dx = -grid;
+      this.dy = 0;
+    }
+  },
+  right: function(){
+    if(this.dx == 0){
+      this.dx = grid;
+      this.dy = 0;
+    }
+  },
+  up: function(){
+    if(this.dy == 0){
+      this.dy = -grid;
+      this.dx = 0;     
+    }
+  },
+  down: function(){
+    if(this.dy == 0){
+      this.dy = grid;
+      this.dx = 0;
     }
   }
-  return false;
 }
 
-function snakeAccidentFaceToFace(){
-  if((snakeA.cells[0].x === snakeB.cells[0].x) && (snakeA.cells[0].y === snakeB.cells[0].y)){
-    return true;
+function Apple(){
+  this.x = getRandomInt();
+  this.y = getRandomInt();
+}
+
+Apple.prototype = {
+  constuctor: Apple,
+  reset: function(){
+    this.x = getRandomInt();
+    this.y = getRandomInt();
+  },
+  drawApple: function(){
+    context.fillStyle = 'red';
+    context.fillRect(this.x, this.y, 15, 15);
   }
-  return false;
 }
 
+var snakeA = new Snake('#552329');
+var snakeB = new Snake('#393947');
+var apple = new Apple();
 
-function snakeEats(snake){
-  snake.cells.forEach(function(cell, index) {
-    context.fillRect(cell.x, cell.y, grid-1, grid-1);
-
-    if (cell.x === apple.x && cell.y === apple.y) {
-      snake.maxCells++;
-      putApple();
-    }
-
-    if(snakeEatsSnake(cell, index, snakeA) || snakeEatsSnake(cell, index, snakeB)){
-        reset(snake);
-    }
-  });
+function getRandomInt() {
+  return (Math.floor(Math.random() * (30 - 0)) + 0) * grid;
 }
 
 function loop() {
   requestAnimationFrame(loop);
-
   if (++count < 8) {
     return;
   }
   count = 0;
-
-  context.clearRect(0,0,canvas.width,canvas.height);
-
-  moveSnakes();
-  snakesHitWall();
-  trackSnakes();
-  removeCellsAsWeMoveAway();
-  drawApple();
-
-  if(snakeAccidentFaceToFace()){
-    reset(snakeA);
-    reset(snakeB);
-  }
-  context.fillStyle = '#552329';
-  snakeEats(snakeA);
-  context.fillStyle = '#393947';
-  snakeEats(snakeB);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  apple.drawApple();
+  snakeA.loop();
+  snakeB.loop();
 }
 
 // listen to keyboard events to move the snake
 document.addEventListener('keydown', function(e) {
-  // left arrow key
-  if (e.which === 37 && snakeA.dx === 0) {
-    snakeA.dx = -grid;
-    snakeA.dy = 0;
+  switch(e.which) {
+    case 37:
+      snakeA.left();
+      break;
+    case 38:
+      snakeA.up();
+      break;
+    case 39:
+      snakeA.right();
+      break;
+    case 40:
+      snakeA.down();
+      break;
   }
-  // up arrow key
-  else if (e.which === 38 && snakeA.dy === 0) {
-    snakeA.dy = -grid;
-    snakeA.dx = 0;
-  }
-  // right arrow key
-  else if (e.which === 39 && snakeA.dx === 0) {
-    snakeA.dx = grid;
-    snakeA.dy = 0;
-  }
-  // down arrow key
-  else if (e.which === 40 && snakeA.dy === 0) {
-    snakeA.dy = grid;
-    snakeA.dx = 0;
-  }  // a key
-  if (e.which === 65 && snakeB.dx === 0) {
-      snakeB.dx = -grid;
-      snakeB.dy = 0;
-  }
-  // w key
-  else if (e.which === 87 && snakeB.dy === 0) {
-    snakeB.dy = -grid;
-    snakeB.dx = 0;
-  }
-  // d key
-  else if (e.which === 68 && snakeB.dx === 0) {
-    snakeB.dx = grid;
-    snakeB.dy = 0;
-  }
-  // s key
-  else if (e.which === 83 && snakeB.dy === 0) {
-    snakeB.dy = grid;
-    snakeB.dx = 0;
+  switch(e.which) {
+    case 65:
+      snakeB.left();
+      break;
+    case 87:
+      snakeB.up();
+      break;
+    case 68:
+      snakeB.right();
+      break;
+    case 83:
+      snakeB.down();
+      break;
   }
 });
 
-
 // start the game
 requestAnimationFrame(loop);
-
 
 Vue.component('draggable-header-view', {
   template: '#header-view-template',
   data: function () {
     return {
       dragging: false,
-      // quadratic bezier control point
       c: { x: 160, y: 160 },
-      // record drag start point
       start: { x: 0, y: 0 }
     }
   },
